dc3d1e0457a62ecb1c6cd4700195edbcb4ef4d76
iddinev <xi_dinev@abv.bg>
2015-06-11 22:20:20 +0300

    Make ordinary dict from (default) list of dicts

--- dependency_resolver.py
+++ dependency_resolver.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
 import json
+import sys
+
+packages_file = open(sys.argv[1], 'r')
+list_pkg = json.load(packages_file)
+
+dict_pkg = {}
+for i in range(len(list_pkg)):
+    def_pkg = dict([(list_pkg[i].get('name'), list_pkg[i]['requires'])])
+    dict_pkg.update(def_pkg)
